e51838d9abeffc36449eebdee5ac7075105b8551
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2024-09-26 21:24:33 +0900

    [add] add screenDate field to ScreenTime entity

--- demo/src/main/java/com/example/moviezip/domain/ScreenTime.java
+++ demo/src/main/java/com/example/moviezip/domain/ScreenTime.java
@@ -8,11 +8,13 @@ public class ScreenTime{
 
     private Date startTime;     // 상영 시작 시간
     private Date endTime;       // 상영 종료 시간
+    private Date screenDate;       // 상영 종료 시간
 
-    public ScreenTime(Long movieId, Long screenId, Date startTime, Date endTime) {
+    public ScreenTime(Long movieId, Long screenId, Date startTime, Date endTime, Date screenDate) {
         this.movieId = movieId;
         this.screenId = screenId;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.screenDate = screenDate;
     }
 }
