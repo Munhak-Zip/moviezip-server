263480775f708eb266ab71525f02b0c485d99052
Dayun Lee <wohd7877@naver.com>
2024-10-02 14:12:11 +0900

    [feat] 영화관 정보 불러오기

---
+++ demo/src/main/java/com/example/moviezip/dao/mybatis/MybatisTheaterDao.java
@@ -0,0 +1,27 @@
+package com.example.moviezip.dao.mybatis;
+
+import com.example.moviezip.dao.TheaterDao;
+import com.example.moviezip.dao.mybatis.mapper.TheaterMapper;
+import com.example.moviezip.domain.ScreenDetail;
+import com.example.moviezip.domain.ScreenTime;
+import com.example.moviezip.domain.Theater;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class MybatisTheaterDao implements TheaterDao {
+    @Autowired
+    private TheaterMapper theaterMapper;
+    @Override
+    public List<Theater> getTheaterByLocation(String location) throws DataAccessException {
+        return theaterMapper.getTheaterByLocation(location);
+    }
+
+    @Override
+    public List<ScreenDetail> getScreeningDetails(Long movieId, String screenDate, String theaterName) throws DataAccessException {
+        return theaterMapper.getScreeningDetails(movieId, screenDate, theaterName);
+    }
+}
