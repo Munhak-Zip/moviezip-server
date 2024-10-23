fc0feb438a7d6f5dbee01d74a187808cd983fcfa
Dayun Lee <wohd7877@naver.com>
2024-10-23 15:33:28 +0900

    [feat] add WebConfig

--- demo/src/main/java/com/example/moviezip/security/WebConfig.java
+++ demo/src/main/java/com/example/moviezip/security/WebConfig.java
@@ -1,7 +1,10 @@
 package com.example.moviezip.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
