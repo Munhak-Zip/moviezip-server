ce1a31f6796f65ae0c9c7f6981f3a04e53c91126
Dayun Lee <wohd7877@naver.com>
2024-10-23 15:47:55 +0900

    [feat] dataSources

--- demo/src/main/java/com/example/moviezip/config/WebSocketConfig.java
+++ demo/src/main/java/com/example/moviezip/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.example.moviezip.config;
+/*package com.example.moviezip.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -24,3 +24,4 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 //.setAllowedOrigins("*");
     }
 }
+*/
