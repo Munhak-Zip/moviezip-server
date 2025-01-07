bff66cc2db540c1b5654f89e3831872e59bf7631
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2025-01-07 19:27:32 +0900

    refact: [add] 토큰에 userId와 roles 클레임 추가 및 JWT에서 userId 추출하는 메서드 추가

--- demo/src/main/java/com/example/moviezip/util/jwtUtil.java
+++ demo/src/main/java/com/example/moviezip/util/jwtUtil.java
@@ -1,5 +1,6 @@
 package com.example.moviezip.util;
 
+import com.example.moviezip.domain.CustomUserDetails;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -29,8 +30,16 @@ public class jwtUtil {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
+
+    //클레임 추출
     private Claims extractAllClaims(String token) {
         return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+
+    //JWT에서 userId 추출
+    public Long extractUserId(String token) {
+        Claims claims = extractAllClaims(token); // JWT에서 모든 claims 가져오기
+        return claims.get("userId", Long.class); // userId 추출 (Long 타입)
     }
 
     private Boolean isTokenExpired(String token) {
@@ -38,16 +47,20 @@ public class jwtUtil {
     }
 
     //사용자 정보를 바탕으로 토큰을 생성하기 위해 createToken을 호출하는 상위 메서드
+    //userId와 roles도 추출
     public String generateToken(UserDetails userDetails) {
+        CustomUserDetails customUser = (CustomUserDetails) userDetails;
         Map<String, Object> claims = new HashMap<>();
+        claims.put("userId", customUser.getUser());
+        claims.put("roles", customUser.getAuthorities());
         return createToken(claims, userDetails.getUsername());
     }
 
     //실제 JWT 토큰을 생성하는 역할을 하는 하위 메서드
     // 이 메서드는 claims와 subject를 받아서 JWT 토큰을 빌드하고 서명하여 반환 함.
-    private String createToken(Map<String, Object> claims, String subject) {
+    private String createToken(Map<String, Object> additionalClaims, String subject) {
         return Jwts.builder()
-                .setClaims(claims) // 클레임 설정
+                .setClaims(additionalClaims) // 추가 클레임 설정
                 .setSubject(subject) // 사용자 이름 (subject)
                 .setIssuedAt(new Date(System.currentTimeMillis())) // 발행 시간
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 만료 시간 (10시간)
