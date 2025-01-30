aaefd97d64795fa6cfaff6d9dd3eacefbb1a4d76
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2025-01-30 13:38:44 +0900

    [feat] 필요없는 주석 제거

--- demo/src/main/java/com/example/moviezip/util/jwtUtil.java
+++ demo/src/main/java/com/example/moviezip/util/jwtUtil.java
@@ -12,8 +12,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -63,8 +61,6 @@ public class jwtUtil {
         return extractExpiration(token).before(new Date());
     }
 
-    //사용자 정보를 바탕으로 토큰을 생성하기 위해 createToken을 호출하는 상위 메서드
-    //userId와 roles도 추출
     public String generateToken(UserDetails userDetails) {
         CustomUserDetails customUser = (CustomUserDetails) userDetails;
         Map<String, Object> claims = new HashMap<>();
@@ -74,13 +70,10 @@ public class jwtUtil {
                 .collect(Collectors.toList());
 
         claims.put("userId", customUser.getUser());
-
         claims.put("roles", roles);
         return createToken(claims, userDetails.getUsername());
     }
 
-    //실제 JWT 토큰을 생성하는 역할을 하는 하위 메서드
-    // 이 메서드는 claims와 subject를 받아서 JWT 토큰을 빌드하고 서명하여 반환 함.
     private String createToken(Map<String, Object> additionalClaims, String subject) {
         return Jwts.builder()
                 .setClaims(additionalClaims) // 추가 클레임 설정
@@ -91,7 +84,6 @@ public class jwtUtil {
                 .compact(); // JWT 토큰 생성
     }
 
-    //refresh토큰 생성
     public String generateRefreshToken(UserDetails userDetails) {
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
@@ -101,31 +93,13 @@ public class jwtUtil {
                 .compact();
     }
 
-    // refreshToken을 발급받은 후, 쿠키에 저장하는 코드
-    public void setRefreshTokenInCookie(String refreshToken, HttpServletResponse response) {
-
-        // Refresh Token을 HttpOnly 쿠키에 저장
-        Cookie cookie = new Cookie("refreshToken", refreshToken);
-        cookie.setHttpOnly(true);    // JavaScript에서 접근 불가
-        cookie.setSecure(false);      // HTTPS 환경에서만 전송되도록 설정 (필요한 경우)
-        cookie.setPath("/");         // 모든 경로에서 사용 가능하도록 설정
-        cookie.setMaxAge((int) REFRESH_TOKEN_EXPIRATION);  // 유효 기간 설정
-
-        response.addCookie(cookie);     // 응답에 쿠키 추가
-        System.out.println("쿠키에 refreshToken 설정: " + refreshToken);
-    }
-
-    //refreshToken 검증 및 AccessToken발급
     public String refreshAccessToken(String refreshToken) {
-        log.info("refreshToken:" + refreshToken);
-        // refreshToken 유효성 검증
+
         if (!validateRefreshToken(refreshToken)) {
-            throw new CustomException(ExceptionStatus.INVALID_TOKEN);  // 유효하지 않은 토큰인 경우 예외 던짐
+            throw new CustomException(ExceptionStatus.INVALID_TOKEN);
         }
-        //클레임 추출
         Claims claims = extractAllClaims(refreshToken);
 
-        //refreshToken의 발행 일자가 현재보다 이후라면?
         if (claims.getIssuedAt().after(new Date())) {
             throw new CustomException(ExceptionStatus.PREMATURE_TOKEN);
         }
@@ -139,15 +113,14 @@ public class jwtUtil {
         String username = extractUsername(refreshToken);
 
         UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
-        return generateToken(userDetails); // 새로운 AccessToken 발급
+        return generateToken(userDetails);
 
     }
 
     public boolean validateRefreshToken(String refreshToken) {
-        // 유효성 검사: JWT가 유효한지, 만료되지 않았는지 등
         try {
             Jwts.parserBuilder()
-                    .setSigningKey(SECRET_KEY)  // JWT 서명 키
+                    .setSigningKey(SECRET_KEY)
                     .build()
                     .parseClaimsJws(refreshToken);
             return true;
