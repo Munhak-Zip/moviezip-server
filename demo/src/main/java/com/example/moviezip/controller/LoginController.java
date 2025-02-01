f3d5a57d218544bdcd905b94f9d7c3a503a58f30
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2025-02-01 16:45:53 +0900

    [feat] LoginController 수정

--- demo/src/main/java/com/example/moviezip/controller/LoginController.java
+++ demo/src/main/java/com/example/moviezip/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.moviezip.controller;
 
 import com.example.moviezip.domain.CustomUserDetails;
 import com.example.moviezip.domain.RefreshToken;
+import com.example.moviezip.domain.User;
 import com.example.moviezip.domain.jwt.AuthenticationRequest;
 import com.example.moviezip.domain.jwt.AuthenticationResponse;
 import com.example.moviezip.service.CustomUserDetailsService;
@@ -16,6 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -37,12 +39,10 @@ public class LoginController {
 
     private final AuthenticationManager authenticationManager;
     private final jwtUtil jwtTokenUtil;
-    private final CustomUserDetailsService userDetailsService;
     private final RefreshTokenService refreshTokenService;
     @Value("${token.refresh-token-expire-time}")
     private long refreshTokenExpirationTime;
-    private final CustomUserDetailsService customUserDetailsService;
-
+    private final CustomUserDetailsService userDetailsService;
 
     //사용자의 id,pw를 검증
     //jwtUtil을 호출해 Token을 생성하고 JwtResponse에 Token을 담아 return ResponseEntity
@@ -51,33 +51,37 @@ public class LoginController {
                                                        HttpServletResponse response) throws Exception {
 
         try {
-            authenticationManager.authenticate(
+            Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
                             authenticationRequest.getUsername(),
                             authenticationRequest.getPassword())
             );
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+            final String accessToken = jwtTokenUtil.generateToken(userDetails);
+
+            RefreshToken refreshToken = refreshTokenService.createOrUpdateRefreshToken(userDetails, refreshTokenExpirationTime, response);
+
+            // Refresh Token을 HttpOnly 쿠키에 저장
+            Cookie cookie = new Cookie("refreshToken", refreshToken.getToken());
+            cookie.setHttpOnly(true);    // JavaScript에서 접근 불가
+            cookie.setSecure(false);      // HTTPS 환경에서만 전송되도록 설정 (필요한 경우)
+            cookie.setPath("/");         // 모든 경로에서 사용 가능하도록 설정
+            cookie.setMaxAge((int) refreshTokenExpirationTime);  // 유효 기간 설정
+            response.addCookie(cookie);
+            log.info("쿠키 설정: refreshToken={}, MaxAge={}", refreshToken.getToken(), refreshTokenExpirationTime);
+            return ResponseEntity.ok(new AuthenticationResponse(accessToken));
 
         } catch (BadCredentialsException e) {
             throw new Exception("Incorrect username or password", e);
         }
 
-        final UserDetails userDetails = userDetailsService
-                .loadUserByUsername(authenticationRequest.getUsername());
-        final String accessToken = jwtTokenUtil.generateToken(userDetails);
-
-        RefreshToken refreshToken = refreshTokenService.createOrUpdateRefreshToken(userDetails, refreshTokenExpirationTime, response);
-
-        // Refresh Token을 HttpOnly 쿠키에 저장
-        Cookie cookie = new Cookie("refreshToken", refreshToken.getToken());
-        cookie.setHttpOnly(true);    // JavaScript에서 접근 불가
-        cookie.setSecure(false);      // HTTPS 환경에서만 전송되도록 설정 (필요한 경우)
-        cookie.setPath("/");         // 모든 경로에서 사용 가능하도록 설정
-        cookie.setMaxAge((int) refreshTokenExpirationTime);  // 유효 기간 설정
-        response.addCookie(cookie);
-        log.info("쿠키 설정: refreshToken={}, MaxAge={}", refreshToken.getToken(), refreshTokenExpirationTime);
+//        final UserDetails userDetails = userDetailsService
+//                .loadUserByUsername(authenticationRequest.getUsername());
+        // 인증된 Authentication 객체에서 UserDetails 가져오기
 
         // accessToken은 JSON 응답으로 보내고
-        return ResponseEntity.ok(new AuthenticationResponse(accessToken));
+
     }
 
     @PostMapping("/refresh")
@@ -102,10 +106,13 @@ public class LoginController {
 
         // 후속 과정으로 유효성 검사 등 계속 진행...
         // refreshToken이 유효하면 새로운 access token 발급
-        String newAccessToken = jwtTokenUtil.refreshAccessToken(refreshToken);
-        CustomUserDetails userDetails = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-        refreshTokenService.createOrUpdateRefreshToken(userDetails, refreshTokenExpirationTime, response);
+        String newAccessToken = null;
+        if(refreshTokenService.validateRefreshToken(refreshToken)){
+            newAccessToken = jwtTokenUtil.refreshAccessToken(refreshToken);
+        }
+
+        //refreshTokenService.createOrUpdateRefreshToken(userDetails, refreshTokenExpirationTime, response);
 
         // access token을 JSON 응답으로 반환
         return ResponseEntity.ok(new AuthenticationResponse(newAccessToken));
